ea60a2c4826b0df00accaebce1b3c9577f364dac
Rudy Kong <rkongtin@sfu.ca>
2019-01-27 11:27:43 -0800

    modified UI

--- app/src/main/java/com/example/nwhacks19/boost/MainActivity.java
+++ app/src/main/java/com/example/nwhacks19/boost/MainActivity.java
@@ -1,46 +1,18 @@
 package com.example.nwhacks19.boost;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-
-import com.snapchat.kit.sdk.SnapLogin;
-import com.snapchat.kit.sdk.core.controller.LoginStateController;
 import android.widget.Button;
 
 import com.snapchat.kit.sdk.SnapCreative;
 import com.snapchat.kit.sdk.creative.api.SnapCreativeKitApi;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final float BITMOJI_CONTAINER_FOCUS_WEIGHT = 9.0f;
-    private static final String EXTERNAL_ID_QUERY = "{me{externalId}}";
-
-    final LoginStateController.OnLoginStateChangedListener mLoginStateChangedListener =
-        new LoginStateController.OnLoginStateChangedListener() {
-            @Override
-            public void onLoginSucceeded() {
-                System.out.println("Logged in");
-            }
-
-            @Override
-            public void onLoginFailed() {
-                // Here you could update UI to show login failure
-            }
-
-            @Override
-            public void onLogout() {
-                // Here you could update UI to reflect logged out state
-            }
-        };
 
     private SnapCreativeKitApi snapCreativeKitApi;
 
@@ -51,18 +23,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setupSnapKitAPI();
-
-        final Button loginButton = findViewById(R.id.LoginButton);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                SnapLogin.getAuthTokenManager(MainActivity.this).startTokenGrant();
-            }
-        });
-        SnapLogin.getLoginStateController(this).addOnLoginStateChangedListener(mLoginStateChangedListener);
-        boolean isUserLoggedIn = SnapLogin.isUserLoggedIn(this);
-        if (isUserLoggedIn) {
-            System.out.println("Logged in");
-        }
 
 
         Button button = (Button) findViewById(R.id.button);
