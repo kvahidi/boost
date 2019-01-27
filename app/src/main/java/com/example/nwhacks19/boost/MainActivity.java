af422ca0cf07bb86ce7f24d3b1fa6da8414d5fce
Rudy Kong <rkongtin@sfu.ca>
2019-01-27 05:41:15 -0800

    fixed merge conflicts

--- app/src/main/java/com/example/nwhacks19/boost/MainActivity.java
+++ app/src/main/java/com/example/nwhacks19/boost/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.nwhacks19.boost;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,8 +10,10 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.View;
+import android.widget.Button;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +21,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        final Button loginButton = findViewById(R.id.LoginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("*/*");
+                intent.setPackage("com.snapchat.android");
+                startActivity(Intent.createChooser(intent, "Open Snapchat"));
             }
         });
     }
